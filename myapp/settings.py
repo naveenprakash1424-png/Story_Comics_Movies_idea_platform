2b8b5707264a84688bc4f3adee23d7c088e6b2c3
naveenprakash1424-png <naveenprakash1424@gmail.com>
2026-03-28 13:30:43 +0530

    Set DEBUG to True in settings.py
    Changed DEBUG setting to True for development.

--- myapp/settings.py
+++ myapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xt2n4!qq!hf2v3e#&*+v=tzd^%s*g%#(4ymu$p!@&v&ub3i_b+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
